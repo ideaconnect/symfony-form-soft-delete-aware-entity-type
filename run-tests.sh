b36aceb814dc4d29e34cf1ca9770f8d6dbf63cbc
Bartosz Pachołek <bartosz+github@idct.tech>
2024-07-16 21:35:25 +0200

    run composer in sf folder

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,8 @@ cd tests/functional \
 && docker compose build \
 && docker compose up -d --build --force-recreate \
 && docker compose exec  --workdir=/app testapp composer install \
+&& docker compose exec  --workdir=/app/tests/functional testapp composer install \
 && docker compose exec  --workdir=/app testapp ./vendor/bin/phpunit \
 && sleep 5 \
 && docker compose exec testapp bin/console doctrine:schema:create \
-&& docker compose exec testapp vendor/bin/behat \
+&& docker compose exec testapp vendor/bin/behat
